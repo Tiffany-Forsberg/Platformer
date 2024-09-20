387d4bbe8404c1d429875d46c70a1b545993d596
Tore Forsberg <erotfor@gmail.com>
2024-09-20 11:07:03 +0200

    Initialize project
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace platformer {
+    class Program {
+        static void Main(string[] args) {
+            using (var window = new RenderWindow(
+                new VideoMode(800, 600),
+                "Platformer"
+            )) {
+                window.Closed += (o, e) => window.Close();
+
+                // TODO: Initialize
+                Clock clock = new Clock();
+
+                while (window.IsOpen) {
+                    window.DispatchEvents();
+                    float deltaTime = clock.Restart().AsSeconds();
+                    // TODO: Updates
+                    window.Clear();
+                    // TODO: Drawing
+                    window.Display();
+                }
+            }
+        }
+    }
+}
