ec1625c9cc813177e58f3ce4e9d8c56d7696cdd4
Oskar Rickardsson <oskar.rickardsson12@gmail.com>
2024-09-20 11:49:50 +0200

    Add scene and entity class
    Co-Authored-By: Tiffany Forsberg <142976914+Tiffany-Forsberg@users.noreply.github.com>

---
+++ Entity.cs
@@ -0,0 +1,42 @@
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Platformer
+{
+    class Entity
+    {
+        private readonly string textureName;
+        protected readonly Sprite sprite;
+        public bool Dead;
+
+        protected Entity(string textureName)
+        {
+            this.textureName = textureName;
+            sprite = new Sprite();
+        }
+
+        public Vector2f Position
+        {
+            get => sprite.Position;
+            set => sprite.Position = value;
+        }
+
+        public virtual FloatRect Bounds => sprite.GetGlobalBounds();
+
+        public virtual void Create(Scene scene)
+        {
+            sprite.Texture = scene.LoadTexture(textureName);
+        }
+
+        public virtual void Update(Scene scene, float deltaTime)
+        {
+            
+        }
+
+        public virtual void Render(RenderTarget target)
+        {
+            target.Draw(sprite);
+        }
+    }
+}
---
+++ Scene.cs
@@ -0,0 +1,60 @@
+using SFML.Graphics;
+
+namespace Platformer
+{
+    class Scene
+    {
+        private readonly Dictionary<string, Texture> textures;
+        private readonly List<Entity> entities;
+
+        Scene()
+        {
+            textures = new Dictionary<string, Texture>();
+            entities = new List<Entity>();
+        }
+
+        public void Spawn(Entity entity)
+        {
+            entities.Add(entity);
+            entity.Create(this);
+        }
+
+        public Texture LoadTexture(string name)
+        {
+            if (textures.TryGetValue(name, out Texture found))
+            {
+                return found;
+            }
+
+            string fileName = $"assets/{name}.png";
+            Texture texture = new Texture(fileName);
+            textures.Add(name, texture);
+            return texture;
+        }
+
+        public void UpdateAll(float deltaTime)
+        {
+            for (int i = entities.Count - 1; i >= 0; i--)
+            {
+                Entity entity = entities[i];
+                entity.Update(this, deltaTime);
+            }
+
+            for (int i = 0; i < entities.Count;)
+            {
+                Entity entity = entities[i];
+                if (entity.Dead) entities.RemoveAt(i);
+                else i++;
+            }
+        }
+
+        public void RenderAll(RenderTarget target)
+        {
+            for (int i = entities.Count - 1; i >= 0; i--)
+            {
+                Entity entity = entities[i];
+                entity.Render(target);
+            }
+        }
+    }
+}
