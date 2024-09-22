01db21cb8db0596fce3e064e251c5fdf147b2370
Oskar Rickardsson <oskar.rickardsson12@gmail.com>
2024-09-22 14:24:57 +0200

    add key class
    Co-Authored-By: Tiffany Forsberg <142976914+Tiffany-Forsberg@users.noreply.github.com>

---
+++ Key.cs
@@ -0,0 +1,14 @@
+using SFML.Graphics;
+using SFML.System;
+
+namespace Platformer
+{
+    public class Key : Entity
+    {
+        public Key() : base("tileset")
+        {
+            sprite.TextureRect = new IntRect(126, 18, 18, 18);
+            sprite.Origin = new Vector2f(9, 9);
+        }
+    }
+}
