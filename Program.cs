a91817ad06ec2cb499e6d8de920a5fbf17e40844
Oskar Rickardsson <oskar.rickardsson12@gmail.com>
2024-09-22 14:46:34 +0200

    Spawn key, door and background
    Co-Authored-By: Tiffany Forsberg <142976914+Tiffany-Forsberg@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -16,11 +16,15 @@ namespace Platformer {
                 Scene scene = new Scene();
 
                 window.SetView(new View(new Vector2f(200, 150), new Vector2f(400,300)));
-                
+
+                // Spawn entities
                 for (int i = 0; i < 10; i++)
                 {
                     scene.Spawn(new Platform { Position = new Vector2f(18 + i * 18, 288) });
                 }
+                scene.Spawn(new Door {Position = new Vector2f(18, 267.5f)});
+                scene.Spawn(new Key{Position = new Vector2f(60, 60)});
+                scene.Spawn(new Background());
 
                 while (window.IsOpen) {
                     window.DispatchEvents();
